06d5c0561c6d34c613e33ee4d1c1787bfe2c3b96
akhil <akhilendrasingh066@gmail.com>
2026-06-16 12:49:23 +0530

    Added db config in .env

--- database/db_config.py
+++ database/db_config.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DB_CONFIG = {
-    "host": "localhost",
-    "database": "fraud_detection",
-    "user": "postgres",
-    "password": "Faizan@postgres",
-    "port": "5432"
+    "host": os.getenv("DB_HOST"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "port": os.getenv("DB_PORT")
 }
